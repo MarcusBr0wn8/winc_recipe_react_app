700137e190d1b5fb234fd5709d2d7374fc3316f5
Marcus Brown <marco@browndesign.nl>
2023-12-15 14:25:47 +0100

    deleted old code

--- src/components/ui/SearchInput.jsx
+++ src/components/ui/SearchInput.jsx
@@ -20,34 +20,3 @@ export const SearchInput = ({ changeFn, ...props }) => {
     ></Input>
   );
 };
-
-// // search input for searchrecipe
-// export const SearchInput = ({ onChange }) => {
-//   return (
-//     <Flex
-//       m='0 auto'
-//       w={{ base: '90%', sm: '70%', md: '70%', lg: '50%' }}
-//       h={{ base: '20px', md: '30px', lg: '40px' }}
-//       align='center'
-//       justify='center'
-//       bgGradient='linear(to-b, yellow.100, orange.600)'
-//       border='none'
-//       outline='none'
-//       borderRadius='10px'
-//       boxShadow='3px,3px,black'
-//     >
-//       <Input
-//         border='none'
-//         outline='none'
-//         h={{ base: '20px', md: '30px', lg: '40px' }}
-//         fontSize={{ base: '0.7rem', md: '1rem', lg: '1.5rem' }}
-//         color='orange.900'
-//         textAlign='center'
-//         type='text'
-//         placeholder='search recipes'
-//         onChange={onChange}
-//       />
-//       {/* <SearchIcon boxSize={{ base: '0.7rem', lg: '1.5rem' }} m='10px' /> */}
-//     </Flex>
-//   );
-// };
